feb2e65e56e8701daab344cfb0c2b5fc32c09dd3
Farkas <farasz@kkszki.hu>
2024-09-23 11:15:25 +0200

    Connection class is reday to use..

---
+++ Connection.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace AutoConsole
+{
+    public class Connection
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connection()
+        {
+            Host = "127.0.0.1";
+            Database = "Cars";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
