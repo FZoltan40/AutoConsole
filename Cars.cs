441c7f95fa828e7318bc9ce71551f00f2856a709
Farkas <farasz@kkszki.hu>
2024-09-23 11:27:50 +0200

    Cars class is cretaed.

---
+++ Cars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AutoConsole
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string License { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace AutoConsole
 {
-    internal class Program
+
+    public class Program
     {
+        public static List<object> carList = new List<object>();
+        public static void feladat1()
+        {
+
+        }
         static void Main(string[] args)
         {
         }
